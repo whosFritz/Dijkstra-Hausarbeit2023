aaea562ef932ca663aad117e904f1a0b72ab5b3e
Fritz Schubert <fritzschubert21@outlook.de>
2023-02-28 17:57:16 +0100

    trying

--- versuch2.py
+++ versuch2.py
@@ -3,20 +3,6 @@ from typing import List, Tuple
 
 
 def dijkstra(start: int, graph: List[List[Tuple[int, int]]]) -> Tuple[List[int], List[int]]:
-    """
-    Calculates the shortest path from a given start node to all other nodes in a graph using Dijkstra's algorithm.
-
-    Args:
-        start (int): The start node.
-        graph (List[List[Tuple[int,int]]]): The graph represented as an adjacency list.
-
-    Returns:
-        Tuple[List[int], List[int]]: A tuple containing two lists:
-            - The first list contains the shortest distances from the start node to all other nodes.
-            - The second list contains the previous node in the path from the start node to all other nodes.
-              If a node is unreachable, its previous node is set to None.
-    """
-
     n = len(graph)
     dist = [float('inf')] * n
     prev = [None] * n
